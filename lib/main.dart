649b9f534186954ef1589aa923e313211b97c33c
Tran Minh Hai <hai@bio-rithm.com>
2021-03-06 12:25:55 +0700

    DetailCTGView Cubit multiple cubit with navigator and check valid image before displaying it

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_storage_s3/amplify_storage_s3.dart';
-// Generated in previous step 
-import 'amplifyconfiguration.dart'; 
+// Generated in previous step
+import 'amplifyconfiguration.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'dart:io';
@@ -14,34 +14,35 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  @override 
+  @override
   State<StatefulWidget> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  bool _amplifyConfigured = false; 
+  bool _amplifyConfigured = false;
 
   @override
   initState() {
-    super.initState(); 
-    _configureAmplify(); 
+    super.initState();
+    _configureAmplify();
   }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: BlocProvider(
-        create: (context) => UploadCubit()..listFiles(),
-        child: _amplifyConfigured ? TodoView() : ListingView(),
-      ),
-    );
+        home: MultiBlocProvider(providers: [
+      BlocProvider(create: (context) => UploadCubit()..listFiles()),
+      BlocProvider(
+        create: (context) => CTGCubit(),
+      )
+    ], child: _amplifyConfigured ? CTGNav() : ListingView()));
   }
 
   void _configureAmplify() async {
     if (!mounted) return;
     try {
-      Amplify.addPlugins([ AmplifyAuthCognito(), AmplifyStorageS3()]);
+      Amplify.addPlugins([AmplifyAuthCognito(), AmplifyStorageS3()]);
       await Amplify.configure(amplifyconfig);
       setState(() {
         _amplifyConfigured = true;
@@ -52,72 +53,105 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
+class CTGNav extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Navigator(
+      pages: [
+        MaterialPage(child: TodoView()),
+      ],
+      onPopPage: (route, result) {
+        return route.didPop(result);
+      },
+    );
+  }
+}
+
 class TodoView extends StatefulWidget {
-  @override 
+  @override
   State<StatefulWidget> createState() => _TodoViewState();
 }
 
 class _TodoViewState extends State<TodoView> {
-  @override 
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text("S3Upload"),),
-      body: BlocBuilder<UploadCubit, UploadState>(builder: (context, state) {
-        if (state is ListFilesSuccess) {
-          return  state.files.isEmpty ? _emptyView() : ListView.builder(
-            itemCount: state.files.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: InkWell(
-                  onTap: () => {
-                    showModalBottomSheet(
-                        context: context,
-                        builder: (context){
-                          return _detailFileView(state.files[index]);
-                        })
-                  },
-                  child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Row(
-                        children: [
-                          _getFileIcon(state.files[index].key.toString()),
-                          Expanded(child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Text(state.files[index].key.toString()),
-                          ))
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child:
-                      Row(
-                        children: [
-                          IconButton(icon: Icon(Icons.cloud_download), onPressed: (){}),
-                          IconButton(icon: Icon(Icons.delete), onPressed: (){})
-                        ],),
-                    )
-                  ],
-                ),
-                ),
-              );
-            },
-          );
-        } else if (state is ListFilesFailure) {
-          return _exceptionView();
-        } else {
-          return ListingView();
-        }
-      },
+      appBar: AppBar(
+        title: Text("S3Upload"),
+      ),
+      body: BlocBuilder<UploadCubit, UploadState>(
+        builder: (context, state) {
+          if (state is ListFilesSuccess) {
+            return state.files.isEmpty
+                ? _emptyView()
+                : ListView.builder(
+                    itemCount: state.files.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: InkWell(
+                          onTap: () => {
+                            BlocProvider.of<CTGCubit>(context)
+                                .getCTG(state.files[index]),
+                            showModalBottomSheet(
+                                context: context,
+                                builder: (context) {
+                                  return CTGDetailView();
+                                }).whenComplete(() {
+                              BlocProvider.of<CTGCubit>(context)
+                                  .popToDataList();
+                            })
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                flex: 3,
+                                child: Row(
+                                  children: [
+                                    _getFileIcon(
+                                        state.files[index].key.toString()),
+                                    Expanded(
+                                        child: Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                          state.files[index].key.toString()),
+                                    ))
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Row(
+                                  children: [
+                                    IconButton(
+                                        icon: Icon(Icons.cloud_download),
+                                        onPressed: () {}),
+                                    IconButton(
+                                        icon: Icon(Icons.delete),
+                                        onPressed: () {})
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+          } else if (state is ListFilesFailure) {
+            return _exceptionView();
+          } else {
+            return ListingView();
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.cloud_upload), onPressed: () {
+        child: Icon(Icons.cloud_upload),
+        onPressed: () {
           BlocProvider.of<UploadCubit>(context).uploadFile();
-      },
+        },
       ),
     );
   }
@@ -128,34 +162,41 @@ class _TodoViewState extends State<TodoView> {
         FutureBuilder<String>(
           future: BlocProvider.of<UploadCubit>(context).downloadFile(item),
           builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-              if (snapshot.hasData) {
-                return Image.network(snapshot.data);
-              } else {
-                return Text("ERROR");
-              }
+            if (snapshot.hasData) {
+              return Image.network(snapshot.data);
+            } else {
+              return Text("ERROR");
+            }
           },
         ),
-        ElevatedButton(onPressed: () {
-          Navigator.of(context).pop();
-        }, child: Text("Back"))
+        ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text("Back"))
       ],
     );
   }
 
-  Widget _getFileIcon(String name){
+  Widget _getFileIcon(String name) {
     String extension = '.' + name.split(".").last;
 
     if ('.jpg, .jpeg, .png'.contains(extension)) {
-      return Icon(Icons.image, color: Colors.blue,);
+      return Icon(
+        Icons.image,
+        color: Colors.blue,
+      );
     }
     return Icon(Icons.archive);
   }
 
   Widget _emptyView() {
-    return Center(child: Text("Not File Yet"),);
+    return Center(
+      child: Text("Not File Yet"),
+    );
   }
 
-  Widget _exceptionView(){
+  Widget _exceptionView() {
     return Center(
       child: Text("Exception"),
     );
@@ -164,23 +205,23 @@ class _TodoViewState extends State<TodoView> {
 
 class ListingView extends StatelessWidget {
   @override
-   Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
       color: Colors.white,
-      child: Center(child: CircularProgressIndicator(),),
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
 
 // Data Repository
 class DataRepository {
-
   Future<GetUrlResult> downloadFile(StorageItem item) async {
     try {
       GetUrlResult result = await Amplify.Storage.getUrl(key: item.key);
       return result;
-
     } on StorageException catch (e) {
       print(e.message);
     }
@@ -190,7 +231,8 @@ class DataRepository {
     Map<String, String> metadata = <String, String>{};
     metadata['name'] = 'rawecg.csv';
     metadata['desc'] = 'a test file';
-    S3UploadFileOptions options = S3UploadFileOptions(accessLevel: StorageAccessLevel.guest, metadata: metadata);
+    S3UploadFileOptions options = S3UploadFileOptions(
+        accessLevel: StorageAccessLevel.guest, metadata: metadata);
     final key = new DateTime.now().toString();
     // Pick a file
     FilePickerResult result = await FilePicker.platform.pickFiles();
@@ -203,21 +245,20 @@ class DataRepository {
             key: key + file.path.split("/").last,
             options: options);
         print("file: ${file.path} ${uploadResult.toString()}");
-      }
-      on StorageException catch (e) {
+      } on StorageException catch (e) {
         print(e.message);
       }
-    } else {
-
-    }
+    } else {}
   }
 
   Future<List<StorageItem>> listFiles() async {
     try {
       ListResult res = await Amplify.Storage.list();
-      List<StorageItem> items =  res.items.where((element) => element.key.toString().isNotEmpty).toList();
+      List<StorageItem> items = res.items
+          .where((element) => element.key.toString().isNotEmpty)
+          .toList();
       return items;
-    } on StorageException catch(e) {
+    } on StorageException catch (e) {
       print(e.message);
     }
   }
@@ -226,13 +267,9 @@ class DataRepository {
 // State
 abstract class UploadState {}
 
-class ListingFiles extends UploadState {
+class ListingFiles extends UploadState {}
 
-}
-
-class ListFilesFailure extends UploadState {
-
-}
+class ListFilesFailure extends UploadState {}
 
 class ListFilesSuccess extends UploadState {
   final List<StorageItem> files;
@@ -264,5 +301,84 @@ class UploadCubit extends Cubit<UploadState> {
   Future<String> downloadFile(StorageItem item) async {
     final GetUrlResult result = await _dataRepository.downloadFile(item);
     return result.url;
+  }
+}
+
+// CTGDetailView
+class CTGDetailView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return BlocBuilder<CTGCubit, CTGAPIState>(builder: (context, state) {
+      if (state is LoadingCTG) {
+        return Container(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      } else if (state is LoadedCTGFailure) {
+        return Container(
+          child: Center(
+            child: Text("Invalid Image File"),
+          ),
+        );
+      } else if (state is LoadedCTGSuccess) {
+        return Container(
+          child: Center(
+            child: Image.network(state.url.url),
+          ),
+        );
+      } else {
+        return Container(
+          child: Center(
+            child: Text("Exception"),
+          ),
+        );
+      }
+    });
+  }
+}
+
+// CTG Repository
+class CTGRepository {
+  Future<GetUrlResult> getCTG(StorageItem item) async {
+    try {
+      GetUrlResult url = await Amplify.Storage.getUrl(key: item.key);
+      return url;
+    } on StorageException catch (e) {
+      print(e.message);
+    }
+  }
+}
+
+// CTGCubit
+abstract class CTGAPIState {}
+
+class LoadingCTG extends CTGAPIState {}
+
+class LoadedCTGSuccess extends CTGAPIState {
+  GetUrlResult url;
+  LoadedCTGSuccess({this.url});
+}
+
+class LoadedCTGFailure extends CTGAPIState {}
+
+class CTGCubit extends Cubit<CTGAPIState> {
+  final _ctgRepository = CTGRepository();
+  CTGCubit() : super(LoadingCTG());
+
+  void getCTG(StorageItem item) async {
+    // Is item an image
+    String extension = '.' + item.key.toString().split(".").last;
+    if ('.jpg, .jpeg, .png'.contains(extension)) {
+      final url = await _ctgRepository.getCTG(item);
+      emit(LoadedCTGSuccess(url: url));
+    } else {
+      emit(LoadedCTGFailure());
+    }
+  }
+
+  void popToDataList() {
+    emit(LoadingCTG());
   }
 }
