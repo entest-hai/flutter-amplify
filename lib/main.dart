2cfcbfca5e256396b3e9ae1b52fbb44d3c3590b6
Tran Minh Hai <hai@bio-rithm.com>
2021-03-11 16:54:43 +0700

    add db and adding HeartRateModel

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyAppState extends State<MyApp> {
         create: (context) => HeartRateCubit()..loadHeartRateFromFile(),
       ),
       BlocProvider(create: (context) => S3Cubit()..listCsvFiles()),
-      BlocProvider(create: (context) => TodoCubit()..fetchTodo()),
+      BlocProvider(create: (context) => TodoCubit()..subscribeTodo()),
     ], child: _amplifyConfigured ? CTGNavTab() : CTGNavTab()));
   }
 
@@ -786,5 +786,41 @@ class TodoCubit extends Cubit<TodoState> {
   void fetchTodo() async {
     final todos = await _todoRepository.fetchTodo();
     emit(LoadedTodoSuccess(todos: todos));
+  }
+
+  void subscribeTodo() async {
+    // load data
+    final todos = await _todoRepository.fetchTodo();
+    emit(LoadedTodoSuccess(todos: todos));
+    // observe changes
+    try {
+      String graphQLDocument = '''subscription OnCreateTodo {
+        onCreateTodo {
+          id
+          name
+          description
+        }
+      }''';
+
+      var operation = Amplify.API.subscribe(
+          request: GraphQLRequest<String>(document: graphQLDocument),
+          onData: (event) {
+            // print('Subscription event data received: ${event.data}');
+            todos.add(Todo.fromJson(
+                jsonDecode(event.data.toString())['onCreateTodo']));
+            emit(LoadedTodoSuccess(todos: todos));
+          },
+          onEstablished: () {
+            print('Subscription established');
+          },
+          onError: (e) {
+            print('Subscription failed with error: $e');
+          },
+          onDone: () {
+            print('Subscription has been closed successfully');
+          });
+    } on ApiException catch (e) {
+      print('Failed to establish subscription: $e');
+    }
   }
 }
